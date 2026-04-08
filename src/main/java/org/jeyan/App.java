9381431b09397e940030f53bcc479d82f40ee0e6
Jeyanth S <jeyan.dev@outlook.com>
2026-04-08 14:06:27 +0530

    Revert "Merging Feat/logs"

--- src/main/java/org/jeyan/App.java
+++ src/main/java/org/jeyan/App.java
@@ -1,13 +1,13 @@
 package org.jeyan;
 
-import org.jeyan.utils.LogUtil;
-
-class Main {
-    public static void main(String[] args) {
-        // sample Usage
-        LogUtil log  = new LogUtil();
-        log.info("This is a information");
-        log.warn("This is a warning");
-        log.error("This is a error");
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
