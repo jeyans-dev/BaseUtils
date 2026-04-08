b25a32c7cff972ae976db6712aad7a0571188087
jeyans-dev <jeyans.dev@outlook.com>
2026-04-08 13:46:27 +0530

    added log util basic info

---
+++ src/main/java/org/jeyan/utils/LogUtil.java
@@ -0,0 +1,85 @@
+package org.jeyan.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Utility class for automatic logging
+ * and customized file savers
+ * check target/logs
+ */
+public class LogUtil {
+
+    // variable initialization
+    private static final Logger logger = Logger.getLogger(LogUtil.class.getName());
+
+    /**
+     * Standard constructor for initializing by default credentials
+     */
+    public LogUtil() {
+        Path path = Paths.get("target/logs/").toAbsolutePath();
+
+
+        File folder = new File(path.toUri());
+
+        try {
+            FileHandler fileHandler = new FileHandler(folder+getFileName());
+            fileHandler.setFormatter(new SimpleFormatter());
+
+            logger.addHandler(fileHandler);
+            logger.info("Added File handler successfully");
+        } catch(IOException ioException) {
+            logger.severe("Adding file handler issue persisting!!");
+        }
+
+    }
+
+    /**
+     * LogUtil.info()
+     * @param msg String for logging information
+     */
+    public void info(String msg) {
+        logger.info(msg);
+    }
+
+    /**
+     * LogUtil.severe()
+     * @param msg String for Logging severe messages
+     */
+    public void severe(String msg) {
+        logger.severe(msg);
+    }
+
+    /**
+     * LogUtil.error()
+     * @param msg String for Logging error
+     */
+    public void error(String msg) {
+        logger.severe(msg);
+    }
+
+    /**
+     * LogUtil.warn()
+     * @param msg String for logging warning message
+     */
+    public void warn(String msg) {
+        logger.warning(msg);
+    }
+
+    /**
+     * Helper function for making standard filenames
+     * @return filename with unique timestamp
+     */
+    private String getFileName() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MMddyy-hhmmss");
+        return "log-"+LocalDate.now().format(dtf)+".log";
+    }
+
+}
